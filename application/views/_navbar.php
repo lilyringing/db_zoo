e4a9fd6ac0fbc3a87124b7427caee736f6258229
lilyringing <lilytsai0918@gmail.com>
2014-01-11 15:17:44 +0800

    update
    add other css file and update UI in file welcome_message

---
+++ application/views/_navbar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="<?php echo base_url('css/bootstrap.min.css');?>" rel="stylesheet" media="screen">
+	
+</head>
+
+<body>
+<!-- Collect the nav links, forms, and other content for toggling -->
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="nav-header">
+  <a class="navbar-brand" href="<?=site_url("/")?>">Zoo</a>
+  </div>
+  
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="<?=site_url("search/animal_info")?>">動物資訊</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">園區資訊 <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="<?=site_url("display/building")?>">展館位置</a></li>
+          <li><a href="<?=site_url("display/building")?>">商店位置</a></li>
+          <!-- <li class="divider"></li>
+          	   <li><a href="#">Separated link</a></li>   -->
+        </ul>
+      </li>
+    </ul>
+    
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="<?=site_url("user/login")?>">登入</a></li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
+</body>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,22 +1,19 @@
-<?php include ("_header.php"); ?>
+<?php include ("_header.php"); 
+	  include ("_navbar.php");?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="<?php echo base_url('css/bootstrap.min.css');?>" rel="stylesheet" media="screen">
 </head>
 
 <body>
 
 <div id="container">
-	<h1>Welcome to Taipei City Zoo!</h1>
-
-	<div id="body">
-		<a href="<?=site_url("user/login")?>" type=button>login</a>
-		<a href="<?=site_url("search/animal_info")?>" type=button>蒐尋動物</a>
-	</div>
 	
 </div>
 
+<script src="https://code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
