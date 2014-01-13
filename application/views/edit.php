35da4e5de55a0fee96a46622f4d6e18102a4714a
johnny12728 <b00705023@ntu.edu.tw>
2014-01-13 11:58:22 +0800

    add view page edit
    now for testing editting function
    it can be merged into other view page in the future

---
+++ application/views/edit.php
@@ -0,0 +1,17 @@
+<?php include ("_header.php");
+	  include ("_navbar.php");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<style type="text/css">
+	</style>
+</head>
+
+<body>
+
+
+
+
+</body>
